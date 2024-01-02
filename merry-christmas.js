62be0d7adcbeacbe300b7d376dcc0924c0ad0a2d
Giovanni Pellicciotta <giovanni.pellicciotta@gmail.com>
2024-01-02 14:28:29 +0100

    Start snowing from beginning

--- merry-christmas.js
+++ merry-christmas.js
@@ -1,7 +1,9 @@
 import * as snowflakes from 'https://www.pellicciotta.com/hinolugi-support.js/js/snowflakes.js';
 import * as log from 'https://www.pellicciotta.com/hinolugi-support.js/js/log.js';
 
-let state = 0; // Not snowing
+let state = 1; // Snowing
+snowflakes.start();
+
 document.addEventListener("click", () => {
   state += 1;
   if (state > 2) { state = 0; }
@@ -12,7 +14,7 @@ document.addEventListener("click", () => {
       break;
     case 1: // Snowing starts:  
       log.info("It starts to snow...")
-      snowflakes.start();
+      snowflakes.start();    
       break;
     case 2: // Snowing stops:
       log.info("Snowing stops now...")
