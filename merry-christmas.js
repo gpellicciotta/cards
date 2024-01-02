b03fad14d1aa1c23120f0bc73279149e073da60d
Giovanni Pellicciotta <giovanni.pellicciotta@gmail.com>
2024-01-02 10:43:46 +0100

    Re-using hinolugi-support.js functions

--- merry-christmas.js
+++ merry-christmas.js
@@ -1,3 +1,7 @@
+import * as log from 'https://www.pellicciotta.com/hinolugi-support.js/js/log.js';
+import * as utils from 'https://www.pellicciotta.com/hinolugi-support.js/js/utils.js';
+import * as dom from 'https://www.pellicciotta.com/hinolugi-support.js/js/domutils.js';
+
 // Constants:
 const snowFlakeCount = 30;
 const snowSymbols = [ "*","❆","❅","❄" ];
@@ -9,20 +13,6 @@ createSnow();
 window.requestAnimationFrame(animateSnow);
 
 // Support functions:
-function randomBetween(min, max) {
-  if (arguments.length === 1) {
-    max = min; // First argument is then max
-    min = 0;
-  }
-  let spread = max - min + 1;
-  return min + Math.floor(spread * Math.random());
-}
-
-function randomElement(arr) {
-  let randomIndex = randomBetween(0, arr.length - 1);  
-  return arr[randomIndex];
-}
-
 function reset() {
   snowFlakes = [];
   document.querySelectorAll("span.snowflake").forEach((el) => { el.remove(); });
@@ -32,17 +22,17 @@ function createSnowFlake() {
   const screenWidth = window.innerWidth;
   // Create new object with meta-data and document element:
   let newElement = document.createElement("span");    
-  let xPos = randomBetween(0, screenWidth);
-  let yPos = 0 - randomBetween(10, screenWidth / 2);
-  let size = randomBetween(16, 32);
-  let zIndex = randomBetween(500, 1000);
+  let xPos = utils.random(0, screenWidth);
+  let yPos = 0 - utils.random(10, screenWidth / 2);
+  let size = utils.random(16, 32);
+  let zIndex = utils.random(500, 1000);
   let newSnowFlake = { 
     id: `snowflake-${snowFlakes.length}`,
     element: newElement,
     xPos: xPos,
     yPos: yPos,
-    dir: randomElement([-1, 1]),
-    fallSpeed: randomElement([1, 2])
+    dir: utils.randomElement([-1, 1]),
+    fallSpeed: utils.randomElement([1, 2])
   };
   snowFlakes.push(newSnowFlake);
   // Style the document el:
@@ -50,16 +40,16 @@ function createSnowFlake() {
   newElement.style.position = "fixed";
   newElement.style.top = `${yPos}px`;
   newElement.style.right = `${xPos}px`;
-  newElement.style.color = randomElement(snowFlakeColors);
+  newElement.style.color = utils.randomElement(snowFlakeColors);
   newElement.style.fontSize = `${size}px`;
   //newElement.style.width = `${size}px`;
   newElement.style.height = `${size}px`;
   newElement.style.zIndex = `${zIndex}`;
   newElement.style.pointerEvents = "none";
-  newElement.innerText = randomElement(snowSymbols); // + `sp:${newSnowFlake.fallSpeed},d:${newSnowFlake.dir}`;
+  newElement.innerText = utils.randomElement(snowSymbols); // + `sp:${newSnowFlake.fallSpeed},d:${newSnowFlake.dir}`;
   // Append to document:
   document.body.appendChild(newElement);
-  console.log(`${snowFlakes.length} snowflakes have been created now`);
+  log.trace(`${snowFlakes.length} snowflakes have been created now`);
 }
 
 function createSnow() {
@@ -80,12 +70,12 @@ function animateSnow() {
     let xPos = snowFlake.xPos;
     let yPos = snowFlake.yPos;
     // Calculate new position
-    xPos += randomBetween(0, 1) * snowFlake.dir;
+    xPos += utils.random(0, 1) * snowFlake.dir;
     yPos += snowFlake.fallSpeed;
     // Check window boundaries: reset
     if (yPos > screenHeight) {
-      xPos = randomBetween(0, screenWidth);
-      yPos = 0 - randomBetween(10, 20);
+      xPos = utils.random(0, screenWidth);
+      yPos = 0 - utils.random(10, 20);
     }
     // Set position (= move the snowflake)
     snowFlake.xPos = xPos;
