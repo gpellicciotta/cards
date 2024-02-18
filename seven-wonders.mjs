01c865ad86e7290e37a469433f421ba7631f1d91
Giovanni Pellicciotta <giovanni.pellicciotta@gmail.com>
2024-02-18 01:18:25 +0100

    Add score card for full game with between 2 and 7 players, while making the duel also still work

--- seven-wonders.mjs
+++ seven-wonders.mjs
@@ -1,89 +1,88 @@
 import * as log from 'https://www.pellicciotta.com/hinolugi-support.js/js/log.mjs';
 
-const pl1NameInput = document.getElementById('player-1-name');
-const pl2NameInput = document.getElementById('player-2-name');
-const pl1BlueCardsInput = document.getElementById('player-1-blue-cards');
-const pl2BlueCardsInput = document.getElementById('player-2-blue-cards');
-const pl1GreenCardsInput = document.getElementById('player-1-green-cards');
-const pl2GreenCardsInput = document.getElementById('player-2-green-cards');
-const pl1YellowCardsInput = document.getElementById('player-1-yellow-cards');
-const pl2YellowCardsInput = document.getElementById('player-2-yellow-cards');
-const pl1PurpleCardsInput = document.getElementById('player-1-purple-cards');
-const pl2PurpleCardsInput = document.getElementById('player-2-purple-cards');
-const pl1WondersInput = document.getElementById('player-1-wonders');
-const pl2WondersInput = document.getElementById('player-2-wonders');
-const pl1GreenCoinsInput = document.getElementById('player-1-green-coins');
-const pl2GreenCoinsInput = document.getElementById('player-2-green-coins');
-const pl1MoneyCoinsInput = document.getElementById('player-1-money-coins');
-const pl2MoneyCoinsInput = document.getElementById('player-2-money-coins');
-const pl1MilitaryInput = document.getElementById('player-1-military');
-const pl2MilitaryInput = document.getElementById('player-2-military');
-const pl1SumInput = document.getElementById('player-1-sum');
-const pl2SumInput = document.getElementById('player-2-sum');
+const playerCount = document.getElementById('player-count');
 
+function recalculate() {
+  let players = playerCount ? Math.min(Math.max(+playerCount.value, 2), 7) : 2; 
+  let maxPoints = 0;
+  for (let i = 1; i <= players; i++) {
+    let blueCardsInput = document.body.querySelector('.card > .row.blue-cards > input.player-' + i);
+    let greenCardsInput = document.body.querySelector('.card > .row.green-cards > input.player-' + i);
+    let yellowCardsInput = document.body.querySelector('.card > .row.yellow-cards > input.player-' + i);
+    let purpleCardsInput = document.body.querySelector('.card > .row.purple-cards > input.player-' + i);
+    let wondersInput = document.body.querySelector('.card > .row.wonders > input.player-' + i);
+    let greenCoinsInput = document.body.querySelector('.card > .row.green-coins > input.player-' + i);
+    let moneyCoinsInput = document.body.querySelector('.card > .row.money-coins > input.player-' + i);
+    let militaryInput = document.body.querySelector('.card > .row.military > input.player-' + i);
+    let sumInput = document.body.querySelector('.card > .row.sum > input.player-' + i);
+    // Calculate:
+    let totalPoints =  (+blueCardsInput.value) + 
+                       (+greenCardsInput.value) +
+                       (+yellowCardsInput.value) +
+                       (+purpleCardsInput.value) +
+                       (+wondersInput.value) +
+                       (+greenCoinsInput.value) +
+                       (+moneyCoinsInput.value) +
+                       (+militaryInput.value);
+    // Assign:
+    sumInput.value = totalPoints;     
+    
+    // Reset winner status:
+    sumInput.classList.remove('winner');
+    let nameInput = document.body.querySelector('.card > .row.players > input.player-' + i);  
+    nameInput.classList.remove('winner');
 
-function sum() {
-  pl1SumInput.value = (+pl1BlueCardsInput.value) + 
-                      (+pl1GreenCardsInput.value) +
-                      (+pl1YellowCardsInput.value) +
-                      (+pl1PurpleCardsInput.value) +
-                      (+pl1WondersInput.value) +
-                      (+pl1GreenCoinsInput.value) +
-                      (+pl1MoneyCoinsInput.value) +
-                      (+pl1MilitaryInput.value);
-
-  pl2SumInput.value = (+pl2BlueCardsInput.value) + 
-                      (+pl2GreenCardsInput.value) +
-                      (+pl2YellowCardsInput.value) +
-                      (+pl2PurpleCardsInput.value) +
-                      (+pl2WondersInput.value) +
-                      (+pl2GreenCoinsInput.value) +
-                      (+pl2MoneyCoinsInput.value) +
-                      (+pl2MilitaryInput.value);       
-
-  pl1NameInput.classList.remove("winner"); 
-  pl2NameInput.classList.remove("winner");                     
-  pl1SumInput.classList.remove("winner");
-  pl2SumInput.classList.remove("winner");                    
-  if (+pl1SumInput.value > +pl2SumInput.value) {
-    pl1NameInput.classList.add("winner"); 
-    pl1SumInput.classList.add("winner");
-  } 
-  else if (+pl1SumInput.value < +pl2SumInput.value) {
-    pl2NameInput.classList.add("winner"); 
-    pl2SumInput.classList.add("winner");  
-  }                   
+    // Check max points:
+    if (totalPoints > maxPoints) {
+      maxPoints = totalPoints;
+    }
+  }
+  // Now check/mark the winner(s):
+  for (let i = 1; i <= players; i++) {
+    let sumInput = document.body.querySelector('.card > .row.sum > input.player-' + i);
+    let nameInput = document.body.querySelector('.card > .row.players > input.player-' + i);    
+    if (+sumInput.value === maxPoints) {
+      sumInput.classList.add('winner');
+      nameInput.classList.add('winner');
+    }
+  }                 
 }
 
-function reset() {
-  pl1NameInput.classList.remove("winner"); 
-  pl2NameInput.classList.remove("winner");                     
-  pl1NameInput.value = "";
-  pl2NameInput.value = "";
-  pl1SumInput.classList.remove("winner");
-  pl2SumInput.classList.remove("winner");
+function resetScores() {
+  for (let i = 1; i <= 7; i++) {
+    let nameInput = document.body.querySelector('.card > .row.players > input.player-' + i);  
+    nameInput.classList.remove('winner');
 
-  pl1BlueCardsInput.value = 0; 
-  pl1GreenCardsInput.value = 0;
-  pl1YellowCardsInput.value = 0;
-  pl1PurpleCardsInput.value = 0;
-  pl1WondersInput.value = 0;
-  pl1GreenCoinsInput.value = 0;
-  pl1MoneyCoinsInput.value = 0;
-  pl1MilitaryInput.value = 0;
-  pl1SumInput.value = 0;
+    let blueCardsInput = document.body.querySelector('.card > .row.blue-cards > input.player-' + i);
+    blueCardsInput.value = 0;
 
-  pl2BlueCardsInput.value = 0; 
-  pl2GreenCardsInput.value = 0;
-  pl2YellowCardsInput.value = 0;
-  pl2PurpleCardsInput.value = 0;
-  pl2WondersInput.value = 0;
-  pl2GreenCoinsInput.value = 0;
-  pl2MoneyCoinsInput.value = 0;
-  pl2MilitaryInput.value = 0;
-  pl2SumInput.value = 0;
+    let greenCardsInput = document.body.querySelector('.card > .row.green-cards > input.player-' + i);
+    greenCardsInput.value = 0;
 
-  pl1NameInput.focus();
+    let yellowCardsInput = document.body.querySelector('.card > .row.yellow-cards > input.player-' + i);
+    yellowCardsInput.value = 0;
+
+    let purpleCardsInput = document.body.querySelector('.card > .row.purple-cards > input.player-' + i);
+    purpleCardsInput.value = 0;
+
+    let wondersInput = document.body.querySelector('.card > .row.wonders > input.player-' + i);
+    wondersInput.value = 0;
+
+    let greenCoinsInput = document.body.querySelector('.card > .row.green-coins > input.player-' + i);
+    greenCoinsInput.value = 0;
+
+    let moneyCoinsInput = document.body.querySelector('.card > .row.money-coins > input.player-' + i);
+    moneyCoinsInput.value = 0;
+
+    let militaryInput = document.body.querySelector('.card > .row.military > input.player-' + i);
+    militaryInput.value = 0;
+
+    let sumInput = document.body.querySelector('.card > .row.sum > input.player-' + i);
+    sumInput.classList.remove('winner');
+    sumInput.value = 0;
+  }
+
+  document.body.querySelector('.card > .row.players > .player-1').focus();
 }
 
 document.querySelectorAll("input[type=number]").forEach((el) => {
@@ -102,16 +101,41 @@ document.querySelectorAll("input[type=number]").forEach((el) => {
       el.value = Math.max(0, +el.value - 1);
       e.preventDefault();
     }
-    sum();
+    recalculate();
   });
   el.addEventListener("change", (e) => {
     if (+el.value < 0) {
       el.value = 0;
     }
-    sum();
+    recalculate();
   });  
 });
 
 document.getElementById("reset-button").addEventListener("click", (e) => {
-  reset();
+  resetScores();
 });
+
+if (playerCount) {
+  playerCount.addEventListener("change", (e) => {
+    updatePlayerCount();
+  });
+  updatePlayerCount();
+}
+
+function updatePlayerCount() {
+  let players = Math.min(Math.max(+playerCount.value, 2), 7); 
+  playerCount.value = players;
+  let oldPlayerCount = +(getComputedStyle(document.body).getPropertyValue('--player-count') || '0');
+  document.body.style.setProperty('--player-count', players);
+  for (let i = 1; i <= 7; i++) {
+    if (i <= players) {
+      document.body.classList.add("player-" + i);
+    }
+    else {
+      document.body.classList.remove("player-" + i);
+    }
+  }
+  if (oldPlayerCount != players) {
+    resetScores();
+  }
+}
